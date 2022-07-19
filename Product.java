3e29203fdf38d7ccf720cc626b690537a890b30d
d-n1sh <104403770+d-n1sh@users.noreply.github.com>
2022-07-19 23:17:41 +0800

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,34 @@
+package com.myapplicationdev.android.demotodolist;
+
+public class Product {
+    String name;
+    double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product" +
+                "Name:" + name + '\'' +
+                "Price:" + price;
+    }
+}
